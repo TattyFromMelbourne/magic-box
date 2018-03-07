ffd0e59bae7bfffb2f76f5ae02e73271c03f6005
TattyFromMelbourne <tlenz@lensoftware.com>
2018-03-07 20:33:13 +1100

    Update composer.json and composer.lock files for higher PHP version

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,27 @@
+<?php return array (
+  'aloha/twilio' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Aloha\\Twilio\\Support\\Laravel\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Twilio' => 'Aloha\\Twilio\\Support\\Laravel\\Facade',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'orchestra/parser' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Orchestra\\Parser\\XmlServiceProvider',
+    ),
+  ),
+);
